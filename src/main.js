5031999167ea8870923b7a639fd48e30cba702ab
YukaKadokura <kadokura00000@gmail.com>
2018-07-25 23:48:29 +0900

    .vueファイルのESLintチェックエラーによりアプリケーションを起動できないのでチェックをスルーするよう修正。

--- src/main.js
+++ src/main.js
@@ -1,23 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue';
+import App from './App.vue';
 
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: "#app",
+  el: '#app',
   data: {
-    total: 0
+    total: 0,
   },
   methods: {
-    incrementTotal: function() {
+    incrementTotal() {
       this.total++;
-    }
+    },
   },
   components: {
-    App
+    App,
   },
-  template: "<App></App>"
+  template: '<App></App>',
 });
