1590f54c339963cdf480787de2fad64e799bcab6
YukaKadokura <kadokura00000@gmail.com>
2018-07-25 20:46:31 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>TOTAL: {{ total }}</div>
+		<like-component message="AAA" v-on:increment="incrementTotal"></like-component>
+		<like-component message="BBB" v-on:increment="incrementTotal"></like-component>
+		<like-component message="CCC" v-on:increment="incrementTotal"></like-component>
+		<like-component v-on:increment="incrementTotal"></like-component>
+  </div>
+</template>
+
+<script>
+import likeComponent from "./components/likeComponent.vue";
+
+export default {
+  name: "app",
+  data: function() {
+    return {
+      total: 0
+    };
+  },
+  methods: {
+    incrementTotal: function() {
+      this.total++;
+    }
+  },
+  components: {
+    likeComponent
+  }
+};
+</script>
---
+++ src/components/likeComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="countUp">{{ message }} {{ count }}</button>
+</template>
+
+<script>
+export default {
+  name: "like-component",
+  data: function() {
+    return {
+      count: 0
+    };
+  },
+  props: {
+    message: {
+      type: String,
+      default: "ZZZ"
+    }
+  },
+  methods: {
+    countUp: function() {
+      this.count++;
+      this.$emit("increment");
+    }
+  }
+};
+</script>
